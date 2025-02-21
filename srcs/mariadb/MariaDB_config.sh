56a034cca03daa54c10fb7fde6d647c5065d4701
codingfrog27 <codingmerlin@gmail.com>
2025-02-21 14:56:01 +0100

    maria DB script and dockerfile

---
+++ srcs/mariadb/MariaDB_config.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#service runs a program without env variables and runs it from /
+service mariadb start
+
+#not for any infinite shenanigans, simply giving MDB time to startup :)
+sleep 5 
+
+
+#usually mariadb (or msql) will open its own shell, 
+#the -e option lets you pass a command directly
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DB}\`;"
+
+
+mariadb -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+
+
+mariadb -e "GRANT ALL PRIVILEGES ON ${MYSQL_DB}.* TO \`${MYSQL_USER}\`@'%';"
+
+
+#applying priveleges
+mariadb -e "FLUSH PRIVILEGES;"
+
+
+#finally a script provided by mariaDB, starting in safe mode means autorestarts on issues, 
+#so less chance of it not working :)
+#and more importantly it's a continues command so no unexpected container closing! B)
+mysqld_safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql'
+#opens default port and will ocnnect to any IP (since container IP's can change)
