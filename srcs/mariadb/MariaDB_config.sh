c7909569d6c9a5cd08005f629d54e23a05fa8094
codingfrog27 <codingmerlin@gmail.com>
2025-02-27 17:36:54 +0100

    echoin env variables and found issue maybe?

--- srcs/mariadb/MariaDB_config.sh
+++ srcs/mariadb/MariaDB_config.sh
@@ -2,6 +2,9 @@
 
 #service runs a program without env variables and runs it from /
 service mariadb start
+echo "MYSQL_DB: ${MYSQL_DB}"
+echo "MYSQL_USER: ${MYSQL_USER}"
+echo "MYSQL_PASSWORD: ${MYSQL_PASSWORD}"
 
 #not for any infinite shenanigans, simply giving MDB time to startup :)
 sleep 5 
@@ -16,7 +19,8 @@ mariadb -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYS
 
 
 mariadb -e "GRANT ALL PRIVILEGES ON ${MYSQL_DB}.* TO \`${MYSQL_USER}\`@'%';"
-
+# .* for all tables in DB
+#@ % means can connect from any host
 
 #applying priveleges
 mariadb -e "FLUSH PRIVILEGES;"
