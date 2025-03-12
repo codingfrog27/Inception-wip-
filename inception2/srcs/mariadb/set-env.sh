7e915b86e3a949913db6a39c97078dbbb6080ab9
codingfrog27 <codingmerlin@gmail.com>
2025-03-12 15:27:16 +0100

    mariadb working in container now connecting w host

--- inception2/srcs/mariadb/set-env.sh
+++ inception2/srcs/mariadb/set-env.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
+mkdir -p /run/mysqld /var/log/mysql /var/lib/mysql
+chown mysql:mysql /run/mysqld /var/log/mysql /var/lib/mysql
+
+#optional, will make script exit if any command fails
 set -e
+
 
 # Replace placeholders in init.sql with environment variables
 envsubst < /docker-entrypoint-initdb.d/init.sql.template > /docker-entrypoint-initdb.d/init.sql
