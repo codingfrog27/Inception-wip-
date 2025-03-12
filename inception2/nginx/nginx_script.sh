3b1c78a40b5bb2917173456cb026f58aff257858
codingfrog27 <codingmerlin@gmail.com>
2025-03-12 18:32:21 +0100

    aaaaahhhhh

---
+++ inception2/nginx/nginx_script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Create a self-signed SSL certificate for TLS (or use your own cert files)
+echo "Creating a self-signed SSL certificate..."
+openssl req \
+    -x509 \
+    -nodes \
+    -days 365 \
+    -newkey rsa:2048 \
+    -subj "/C=NL/ST=Holland/L=Amsterdam/O=Codam/CN=${DOMAIN_NAME}" \
+    -out "/etc/nginx/ssl/selfsigned.crt" \
+    -keyout "/etc/nginx/ssl/selfsigned.key"
+
+# Start NGINX in the foreground
+echo "Starting nginx..."
+exec nginx -g "daemon off;"
