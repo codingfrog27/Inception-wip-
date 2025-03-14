cac13939d6e8c7bf39b096de5472cdcd3ad9a150
codingfrog27 <codingmerlin@gmail.com>
2025-03-14 18:57:04 +0100

    idk bro

--- inception2/srcs/nginx/nginx_script.sh
+++ inception2/srcs/nginx/nginx_script.sh
@@ -3,13 +3,13 @@
 # Create a self-signed SSL certificate for TLS (or use your own cert files)
 echo "Creating a self-signed SSL certificate..."
 openssl req \
-    -x509 \
-    -nodes \
-    -days 365 \
-    -newkey rsa:2048 \
-    -subj "/C=NL/ST=Holland/L=Amsterdam/O=Codam/CN=${DOMAIN_NAME}" \
-    -out "/etc/nginx/ssl/selfsigned.crt" \
-    -keyout "/etc/nginx/ssl/selfsigned.key"
+	-x509 \
+	-nodes \
+	-days 365 \
+	-newkey rsa:2048 \
+	-subj "/C=NL/ST=Holland/L=Amsterdam/O=Codam/CN=${DOMAIN_NAME}" \
+	-out "/etc/nginx/ssl/selfsigned.crt" \
+	-keyout "/etc/nginx/ssl/selfsigned.key"
 
 # Start NGINX in the foreground
 echo "Starting nginx..."
